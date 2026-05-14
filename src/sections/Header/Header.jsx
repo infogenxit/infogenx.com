f55cc77870c7dcf81a120c1bbfd3a65ff1dc2853
mdyasar49 <mohamedyasar081786@gmail.com>
2026-05-14 17:11:24 +0530

    fix: resolve Header.jsx syntax errors and complete full menu synchronization

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Header.css?v=202605141706";
+import "./Header.css?v=202605141708";
 import logo from "../../assets/images/logo.webp";
 import ServicesDropdown from "./ServicesDropdown";
 import SolutionsDropdown from "./SolutionsDropdown";
