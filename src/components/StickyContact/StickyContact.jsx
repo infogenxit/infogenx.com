44eb92db3e9c6c0066aaf936655e65a3f2c70f6e
mdyasar49 <mohamedyasar081786@gmail.com>
2026-05-15 19:23:06 +0530

    Updated StickyContact with icons and specific classes

--- src/components/StickyContact/StickyContact.jsx
+++ src/components/StickyContact/StickyContact.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { RiPhoneFill, RiMailSendLine } from "react-icons/ri";
 import "./StickyContact.css";
 
 const StickyContact = () => {
   return (
     <div className="sticky-mobile-contact">
-      <Link to="/contact-us">Contact Us</Link>
-      <a href={`tel:${(process.env.REACT_APP_CONTACT_PHONE || '+61 403 331 910').replace(/\s+/g, '')}`}>
-        Call Now
+      <Link to="/contact-us" className="sticky-btn-contact">
+        <RiMailSendLine /> CONTACT US
+      </Link>
+      <a href={`tel:${(process.env.REACT_APP_CONTACT_PHONE || '+61 403 331 910').replace(/\s+/g, '')}`} className="sticky-btn-call">
+        <RiPhoneFill /> CALL NOW
       </a>
     </div>
   );
