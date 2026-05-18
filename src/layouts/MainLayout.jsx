b47f86200debcf55b7cf4be383bdaf21e15c979d
mdyasar49 <mohamedyasar081786@gmail.com>
2026-05-18 22:22:45 +0530

    fix: correct case-sensitivity of Analytics component import

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "../sections/Footer/Footer";
 import StickyContact from "../components/StickyContact/StickyContact";
 import ScrollToTop from "../components/ScrollTop/ScrollToTop";
 import RouteSeo from "../components/SEO/RouteSeo";
-import GtmPageViewTracker from "../components/analytics/GtmPageViewTracker";
+import GtmPageViewTracker from "../components/Analytics/GtmPageViewTracker";
 
 const MainLayout = ({ children }) => {
   return (
